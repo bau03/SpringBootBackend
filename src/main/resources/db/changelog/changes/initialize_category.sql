398644bc024eca0f2d4818a05d5465b13fef9361
byalcin <mbatuhanyalcin006@hotmail.com>
2020-11-17 14:47:36 +0300

    bug fix

--- src/main/resources/db/changelog/changes/initialize_category.sql
+++ src/main/resources/db/changelog/changes/initialize_category.sql
@@ -1,4 +1,4 @@
-insert into content_category (category_name) values ('bilim')
-insert into content_category (category_name) values ('teknoloji')
-insert into content_category (category_name) values ('oyun')
-insert into content_category (category_name) values ('saglik')
+insert into content_category (category_name) values ('bilim');
+insert into content_category (category_name) values ('teknoloji');
+insert into content_category (category_name) values ('oyun');
+insert into content_category (category_name) values ('saglik');
