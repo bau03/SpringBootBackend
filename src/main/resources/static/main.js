785bd48f36b83e1b9cbe78d2764546867b8f3829
byalcin <mbatuhanyalcin006@hotmail.com>
2020-11-18 09:35:38 +0300

    bug fix

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -3376,7 +3376,7 @@ const Login = () => {
     className: "mb-3 mt-3"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(StyledAnchorTag, {
     className: "btn btn-outline-dark alert-dismissible",
-    href: "https://socialteam.herokuapp.com/oauth2/authorize/google?redirect_uri=https://socialteam.herokuapp.com/auth"
+    href: "https://socialteam.herokuapp.com/oauth2/authorize/google?redirect_uri=https://socialteam.herokuapp.com/#/auth"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_fortawesome_react_fontawesome__WEBPACK_IMPORTED_MODULE_8__["FontAwesomeIcon"], {
     icon: _fortawesome_free_brands_svg_icons__WEBPACK_IMPORTED_MODULE_9__["faGoogle"],
     style: {
